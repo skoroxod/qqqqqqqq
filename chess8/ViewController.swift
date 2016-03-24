0c8e4a6a72d95b2a2d80d315769c6f338b1a591b
Aleksand Skorohodov <skoroxod@mail.ru>
2016-03-24 20:58:04 +0500

    Initial Commit

---
+++ chess8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chess8
+//
+//  Created by Aleksandr Skorokhodov on 24.03.16.
+//  Copyright © 2016 Aleksandr Skorokhodov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ chess8Tests/chess8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  chess8Tests.swift
+//  chess8Tests
+//
+//  Created by Aleksandr Skorokhodov on 24.03.16.
+//  Copyright © 2016 Aleksandr Skorokhodov. All rights reserved.
+//
+
+import XCTest
+@testable import chess8
+
+class chess8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
